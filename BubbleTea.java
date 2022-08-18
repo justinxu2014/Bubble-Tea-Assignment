d031ee3723e111aab4b25e9f8c2a0737f3398773
Justin Xu <60406099+justinxu2014@users.noreply.github.com>
2022-08-18 00:55:16 -0400

    Upload Initial Files

---
+++ BubbleTea.java
@@ -0,0 +1,52 @@
+/**
+    This class represents a bubble tea. - https://www.w3schools.com/java/java_classes.asp
+
+    Tasks: 
+    - Implement the constructor
+    - Parameters to store in constructor: 
+        - name(String)  <- name of drink
+        - price(double) <- price of drink
+        - size(character) <- R, L (Regular, Large)
+        - sugar(double) <- 100%, 75%, 50%, 25%
+        - ice(double) <- 100%, 75%, 50%, 25%
+        - bubbles(boolean) <- true, false
+    - Implement getter methods
+*/
+public class BubbleTea {
+    
+    // Instance variables - https://www.tutorialspoint.com/Instance-variables-in-Java
+    private String name; 
+    private double price, sugar, ice;
+    private boolean bubbles;
+    private char size;
+    
+    // Bubble tea constructor - https://www.w3schools.com/java/java_constructors.asp
+    public BubbleTea() {
+        // TODO
+    }
+
+    // Returns the name
+    public String getName() {
+       // TODO
+    }
+
+    // Returns the price
+    public double getPrice(){
+        // TODO
+    }
+
+    // Returns the size
+    public char getSize(){
+        // TODO
+    }
+    
+    // Returns the surgar percentage
+    public double getSugar() {
+        // TODO
+    }
+    
+    // Returns the ice percentage
+    public double getIce() {
+        // TODO
+    }
+}
---
+++ Driver.java
@@ -0,0 +1,10 @@
+/**
+    Tasks:
+    - Create a bubble tea object.
+    - Print out details for bubble tea.
+ */
+public class Driver {
+    public static void main(String[] args) {
+        
+    }
+}
